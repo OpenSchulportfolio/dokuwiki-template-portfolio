95943fa4552239e21360169a77020cfcec6eaef1
root <root@linuxmuster.lokal>
2012-04-03 18:52:28 +0200

    Backup: Forked from origin, added local modifications

--- main.php
+++ main.php
@@ -1,24 +1,32 @@
 <?php
 /**
- * DokuWiki Default Template 2012
+ * OpenSchulportfolio ("osp") Template
+ * Based on "Dokuwiki Template 2012"
  *
  * @link     http://dokuwiki.org/template
  * @author   Anika Henke <anika@selfthinker.org>
  * @author   Clarence Lee <clarencedglee@gmail.com>
+ * @author   Frank Schiebel <frank@linuxmuster.net>
  * @license  GPL 2 (http://www.gnu.org/licenses/gpl.html)
  */
 
 if (!defined('DOKU_INC')) die(); /* must be run from within DokuWiki */
+
 @require_once(dirname(__FILE__).'/tpl_functions.php'); /* include hook for template functions */
+@require_once(dirname(__FILE__).'/mod/osp_functions.php'); /* include hook for osp specific template functions */
+
+
+
 
 $showSidebar = $conf['sidebar'] && page_exists($conf['sidebar']) && ($ACT=='show');
+$showSidebar = true;
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $conf['lang'] ?>"
   lang="<?php echo $conf['lang'] ?>" dir="<?php echo $lang['direction'] ?>">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" /><![endif]-->
     <title><?php tpl_pagetitle() ?> [<?php echo strip_tags($conf['title']) ?>]</title>
     <?php tpl_metaheaders() ?>
     <meta name="viewport" content="width=device-width,initial-scale=1" />
@@ -27,14 +35,7 @@ $showSidebar = $conf['sidebar'] && page_exists($conf['sidebar']) && ($ACT=='show
 </head>
 
 <body>
-    <?php /* with these Conditional Comments you can better address IE issues in CSS files,
-             precede CSS rules by #IE7 for IE7 and #IE8 for IE8 (div closes at the bottom) */ ?>
     <!--[if lte IE 7 ]><div id="IE7"><![endif]--><!--[if IE 8 ]><div id="IE8"><![endif]-->
-
-    <?php /* the "dokuwiki__top" id is needed somewhere at the top, because that's where the "back to top" button/link links to */ ?>
-    <?php /* classes mode_<action> are added to make it possible to e.g. style a page differently if it's in edit mode,
-         see http://www.dokuwiki.org/devel:action_modes for a list of action modes */ ?>
-    <?php /* .dokuwiki should always be in one of the surrounding elements (e.g. plugins and templates depend on it) */ ?>
     <div id="dokuwiki__site"><div id="dokuwiki__top"
         class="dokuwiki site mode_<?php echo $ACT ?> <?php echo ($showSidebar) ? 'hasSidebar' : ''; ?>">
 
@@ -45,28 +46,35 @@ $showSidebar = $conf['sidebar'] && page_exists($conf['sidebar']) && ($ACT=='show
             <?php if($showSidebar): ?>
                 <!-- ********** ASIDE ********** -->
                 <div id="dokuwiki__aside"><div class="pad include group">
-                    <?php tpl_flush() /* flush the output buffer */ ?>
+                    <?php tpl_flush() ?>
                     <?php _tpl_include('sidebarheader.html') ?>
-                    <?php tpl_include_page($conf['sidebar']) /* includes the given wiki page */ ?>
+                    <?php tpl_include_page($conf['sidebar']) ?>
                     <?php _tpl_include('sidebarfooter.html') ?>
                 </div></div><!-- /aside -->
             <?php endif; ?>
 
             <!-- ********** CONTENT ********** -->
             <div id="dokuwiki__content"><div class="pad group">
-
-                <div class="pageId"><span><?php echo hsc($ID) ?></span></div>
-
                 <div class="page group">
-                    <?php tpl_flush() ?>
-                    <?php _tpl_include('pageheader.html') ?>
-                    <!-- wikipage start -->
-                    <?php tpl_content() /* the main content */ ?>
-                    <!-- wikipage stop -->
-                    <?php _tpl_include('pagefooter.html') ?>
+                    <div id="topbar">
+                        <?php _osp_topbar(); ?>
+                    </div>
+                    <?php if($conf['youarehere']): ?>
+                    <div class="breadcrumbs">
+                        <div class="youarehere"><?php tpl_youarehere() ?></div>
+                    </div>
+                    <?php endif ?>
+                    <div class="content">
+                        <?php tpl_flush() ?>
+                        <?php _tpl_include('pageheader.html') ?>
+                        <!-- wikipage start -->
+                        <?php tpl_content() ?>
+                        <!-- wikipage stop -->
+                        <?php _tpl_include('pagefooter.html') ?>
+                    </div>
                 </div>
 
-                <div class="docInfo"><?php tpl_pageinfo() /* 'Last modified' etc */ ?></div>
+                <div class="docInfo"><?php tpl_pageinfo() ?></div>
 
                 <?php tpl_flush() ?>
             </div></div><!-- /content -->
