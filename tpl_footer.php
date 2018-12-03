1a381fb05f4e21c90800eafc7b94d2bda462ad5b
Andreas Gohr <andi@splitbrain.org>
2018-12-03 17:04:07 +0100

    first go at refactoring into a sub template
    We want to inherit as much as possible from the default template. We
    only need copies of images that are referenced directly (and not from
    CSS).

--- tpl_footer.php
+++ tpl_footer.php
@@ -16,17 +16,20 @@ if (!defined('DOKU_INC')) die();
             tpl_license('button', true, false, false); // license button, no wrapper
             $target = ($conf['target']['extern']) ? 'target="'.$conf['target']['extern'].'"' : '';
         ?>
-        <a href="http://www.openschulportfolio.de" title="Openschulportfolio" target="_blank"><img 
-            src="<?php echo tpl_basedir(); ?>images/button-osp.gif" width="80" height="15" title="Openschulportfolio" alt="Openschulportfolio" border="0" /></a>
-        <a href="http://dokuwiki.org/" title="Driven by DokuWiki" <?php echo $target?>><img
-            src="<?php echo tpl_basedir(); ?>images/button-dw.png" width="80" height="15" alt="Driven by DokuWiki" /></a>
-        <a href="http://www.php.net" title="Powered by PHP" <?php echo $target?>><img
-            src="<?php echo tpl_basedir(); ?>images/button-php.gif" width="80" height="15" alt="Powered by PHP" /></a>
-        <a href="http://validator.w3.org/check/referer" title="Valid XHTML 1.0" <?php echo $target?>><img
-            src="<?php echo tpl_basedir(); ?>images/button-xhtml.png" width="80" height="15" alt="Valid XHTML 1.0" /></a>
-        <a href="http://jigsaw.w3.org/css-validator/check/referer?profile=css3" title="Valid CSS" <?php echo $target?>><img
-            src="<?php echo tpl_basedir(); ?>images/button-css.png" width="80" height="15" alt="Valid CSS" /></a>
+        <a href="http://www.openschulportfolio.de" title="Openschulportfolio" <?php echo $target?>><img
+            src="<?php echo tpl_basedir(); ?>images/button-osp.gif" width="80" height="15" alt="Openschulportfolio" /></a>
+        <a href="https://www.dokuwiki.org/donate" title="Donate" <?php echo $target?>><img
+            src="<?php echo tpl_basedir(); ?>../dokuwiki/images/button-donate.gif" width="80" height="15" alt="Donate" /></a>
+        <a href="https://php.net" title="Powered by PHP" <?php echo $target?>><img
+            src="<?php echo tpl_basedir(); ?>../dokuwiki/images/button-php.gif" width="80" height="15" alt="Powered by PHP" /></a>
+        <a href="//validator.w3.org/check/referer" title="Valid HTML5" <?php echo $target?>><img
+            src="<?php echo tpl_basedir(); ?>../dokuwiki/images/button-html5.png" width="80" height="15" alt="Valid HTML5" /></a>
+        <a href="//jigsaw.w3.org/css-validator/check/referer?profile=css3" title="Valid CSS" <?php echo $target?>><img
+            src="<?php echo tpl_basedir(); ?>../dokuwiki/images/button-css.png" width="80" height="15" alt="Valid CSS" /></a>
+        <a href="https://dokuwiki.org/" title="Driven by DokuWiki" <?php echo $target?>><img
+            src="<?php echo tpl_basedir(); ?>../dokuwiki/images/button-dw.png" width="80" height="15" alt="Driven by DokuWiki" /></a>
     </div>
 </div></div><!-- /footer -->
 
-<?php tpl_includeFile('footer.html') ?>
+<?php
+tpl_includeFile('footer.html');
